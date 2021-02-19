bcdd9eb2b2d84d940dfe1d4a58ddfedeac62a3e7
JUJU <julienhermange@gmail.com>
2021-02-19 13:31:04 +0100

    Teleportation UI V1

---
+++ Assets/Scripts/Julien/UI_Teleport.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Teleport : MonoBehaviour
+{
+    public GameObject Target;
+
+    public GameObject TPLoc1;
+    public GameObject TPLoc2;
+    public GameObject TPLoc3;
+    
+
+    public void Activity1()
+    {
+        Target.gameObject.transform.position = TPLoc1.gameObject.transform.position;
+    }
+
+    public void Activity2()
+    {
+        Target.gameObject.transform.position = TPLoc2.gameObject.transform.position;
+    }
+    public void Activity3()
+    {
+        Target.gameObject.transform.position = TPLoc3.gameObject.transform.position;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
