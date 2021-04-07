168e4e2d473475f3fda76f85c828c49136409172
kevinhuart <huart.ke@gmail.com>
2021-04-07 15:34:48 +0200

    Ajout animation test sphere + renommage script animateOnClick

--- Assets/Scripts/Kevin/animateOnClick.cs
+++ Assets/Scripts/Kevin/animateOnClick.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class onClickBehavior : MonoBehaviour
+public class animateOnClick : MonoBehaviour
 {
-    public GameObject test;
-    
     // Start is called before the first frame update
     void Start()
     {
